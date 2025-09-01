1a56e2b24c2d934ecd34e963ccf2b978de3b7490
Amine-er <errabi.ami@gmail.com>
2025-09-01 13:37:07 +0100

    Setup auth layouts and Social auth form with Next auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { auth } from "@/auth";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold font-inter"></h1>
+    </>
+  );
+};
+
+export default Home;
